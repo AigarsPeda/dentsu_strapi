48a32f556c42a4a20b7c5d18b785bb24ee6f93e0
Aigars <aigarspeda@gmail.com>
2024-09-02 16:42:18 +0300

    chore: Remove commented code in webpack.config.js

--- src/admin/webpack.config.js
+++ src/admin/webpack.config.js
@@ -3,19 +3,6 @@ import path from "path";
 
 /* eslint-disable no-unused-vars */
 module.exports = (config, webpack) => {
-  // console.log("config is", config);
-
-  // plugins: [
-  //   new HtmlWebpackPlugin({
-  //     template: path.resolve(__dirname, './.strapi/client/my_index.html'), // Set the path to your custom HTML file
-  //     filename: 'index.html', // The output file name
-  //     inject: true, // Inject all assets into the given template
-  //   }),
-  //   // Your other plugins
-  // ],
-
-  console.log("config is ?????? ", config.plugins);
-
   // update HtmlWebpackPlugin config to use custom template
   config.plugins.forEach((plugin) => {
     if (plugin.constructor.name === "HtmlWebpackPlugin") {
