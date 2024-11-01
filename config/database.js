c50231366cdf3caa0e55ea012f7c63b410a2fcbe
A.Peda <aigarspeda@gmail.com>
2024-11-01 16:05:12 +0200

    Staff

--- config/database.js
+++ config/database.js
@@ -10,6 +10,15 @@ module.exports = ({ env }) => ({
         env("DATABASE_FILENAME", ".tmp/data.db")
       ),
     },
+    pool: {
+      min: 0,
+      max: 50,
+      acquireTimeoutMillis: 600000,
+      createTimeoutMillis: 300000,
+      idleTimeoutMillis: 300000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 2000,
+    },
     useNullAsDefault: true,
   },
 });
