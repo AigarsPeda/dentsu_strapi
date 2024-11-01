47a0cba802b0af51184e351494b1702bd1557561
A.Peda <aigarspeda@gmail.com>
2024-11-01 14:28:27 +0200

    Refactor database configuration for improved readability and add connection pool settings

--- config/database.js
+++ config/database.js
@@ -1,10 +1,22 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "sqlite",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
+    },
+    pool: {
+      min: 2,
+      max: 20,
+      acquireTimeoutMillis: 600000,
+      createTimeoutMillis: 300000,
+      idleTimeoutMillis: 300000,
+      propagateCreateError: false,
     },
     useNullAsDefault: true,
   },
